6d1943a10bc9fd49cbd658f680e7eeb6ac800c85
Пантюша <alt.z1-fbb2ckl@yopmail.com>
2017-04-02 18:42:07 +0300

    Remove hardcoded IP

--- finder.py
+++ finder.py
@@ -188,7 +188,7 @@ def ipsearch():
                 if port == 21:
                     data = get_ftp_response(ip)
                 else:
-                    data = get_http_response('213.178.227.141', 80)
+                    data = get_http_response(ip, 80)
 
                 if data:
                     logger.info("IP found: {}:{}".format(ip, port))
